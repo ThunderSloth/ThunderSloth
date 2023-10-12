48d78a790f5e78f8381e5b84b7b7d315cd22ba65
Eli Bell <elibell@Elis-MacBook-Pro.local>
2023-10-12 01:44:47 -0400

    .

--- update.py
+++ update.py
@@ -3,5 +3,5 @@
 # 2023-10-11
 import lichess
 lichess_stats = lichess.Lichess()
-lichess_badge = "![LICHESS BADGE](https://img.shields.io/badge/-{}%3A%20{}-gray?style=plastic&logo=lichess&label={}&labelColor=black&color=gray)".format(lichess_stats.username, lichess_stats.time_format, lichess_stats.rating)
+lichess_badge = "![LICHESS BADGE](https://img.shields.io/badge/-{}%3A%20{}-gray?style=plastic&logo=lichess&label={}&labelColor=black&color=gray)".format(lichess_stats.rating, lichess_stats.time_format, lichess_stats.username)
 print(lichess_badge)
