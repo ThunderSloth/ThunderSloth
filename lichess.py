43fe3be94d700ab431b96ff4c7c9313ca636404f
Eli Bell <elibell@Elis-MacBook-Pro.local>
2023-10-12 00:52:09 -0400

    lichess badge

---
+++ lichess.py
@@ -0,0 +1,33 @@
+# This script creates a markdown badge using the lichess and shields.io APIs to display my current chess rating
+# Eli Bell
+# 2023-10-10
+
+import requests
+import json
+import os
+
+class Lichess():
+    def __init__(self, username='elib', time_format='blitz'):
+        self.username = username
+        self.time_format = time_format
+        self._rating = '????'
+    @property
+    def username:
+        return self.username
+    @property
+    def time_format:
+        return self.time_format
+    @property
+    def rating(self):
+        return self._rating
+    @rating.setter
+    def rating(self):
+        try:
+            api_key =  os.environ['LICHESS_API_KEY']
+            endpoint = "https://lichess.org/api/user/"
+            headers = {"Authorization": f"Bearer {api_key}"}
+            response = requests.get(f"{endpoint}{USER}", headers=headers)
+            assert(response.status_code == 200) # check id successful 
+            data = response.json()
+            perfs = data.get("perfs", {})
+            self._rating = perfs[FORMAT]['rating'] # extract rating for specified time-format
--- update.py
+++ update.py
@@ -1,5 +1,7 @@
 # This script generates new markdown content for my GitHub profile README
 # Eli Bell
 # 2023-10-11
-
-print("hello world!")
+import lichess
+lichess_stats = Lichess()
+lichess_badge = "![LICHESS BADGE](https://img.shields.io/badge/-{}%3A%20{}-gray?style=plastic&logo=lichess&label={}&labelColor=black&color=gray)".format(lichess_stats.user, lichess_stats.format, lichess_stats.rating)
+print(lichess_badge)
