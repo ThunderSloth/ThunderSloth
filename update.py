d2935f998760c5749ef5b793929462d42a52bff8
Eli Bell <elibell@elibell.net>
2023-10-12 00:59:54 -0400

    Update update.py

--- update.py
+++ update.py
@@ -2,7 +2,6 @@
 # Eli Bell
 # 2023-10-11
 import lichess
-print("test")
 lichess_stats = Lichess()
 lichess_badge = "![LICHESS BADGE](https://img.shields.io/badge/-{}%3A%20{}-gray?style=plastic&logo=lichess&label={}&labelColor=black&color=gray)".format(lichess_stats.user, lichess_stats.format, lichess_stats.rating)
 print(lichess_badge)
