2a1c104c81e1f0f8dcd8ab644724f33c93159889
Eli Bell <elibell@Elis-MacBook-Pro.local>
2023-10-12 23:20:32 -0400

    progress

--- update.py
+++ update.py
@@ -1,15 +1,15 @@
 # This script generates new markdown content for my GitHub profile README
 # Eli Bell
 # 2023-10-11
-import lichess
+import Lichess from lichess as lc
 
 class Badge():
     def __init__(self, name, url):
         self._name = name
         self._url = url
-        self._badge = f"![{name.upper()}]({url})"
+        self._markdown = f"![{name.upper()}]({url})"
     @property
-    def badge(self):
+    def markdown(self):
         return self._badge
 
-print(Badge("lichess", Lichess.shield).badge)
+print(Badge("lichess", lc.shield).markdown)
