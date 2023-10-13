6fb7eb6b193b9d8c62eab1d58ddf29f5aca8bd9c
Eli Bell <elibell@Elis-MacBook-Pro.local>
2023-10-13 01:18:53 -0400

    working?

--- lichess.py
+++ lichess.py
@@ -7,27 +7,37 @@ import os
 
 class Lichess():
     def __init__(self, username='elib', time_format='blitz'):
+        self._api_key = self.api_key()
         self._username = username
         self._time_format = time_format
         self._rating = self.rating()
         self._shield = self.shield()
+    def api_key(self):
+        key = ''
+        file_path = "lichess_api_key.txt"
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as file:
+                file_contents = file.read()
+            key = file_contents.strip()
+        else:
+            key = os.environ['LICHESS_API_KEY']
+        return key    
     def rating(self):
         unknown = '%3F'*4 # encoding for 4 question marks "????" 
         try:
-            api_key = os.environ['LICHESS_API_KEY']
             endpoint = f"https://lichess.org/api/user/{self._username}"
-            headers = {"Authorization": f"Bearer {api_key}"}
+            headers = {"Authorization": f"Bearer {self._api_key}"}
             response = requests.get(endpoint, headers=headers)
             assert response.status_code == 200  # check if the request was successful
             data = response.json()
             perfs = data.get("perfs", {})
             return perfs.get(self._time_format, {}).get("rating", unknown)  # extract rating for the specified time format
         except Exception as e:
+            print(e)
             return unknown # return a default value in case of an error
     def shield(self):
-       return "https://img.shields.io/badge/-{}%3A%20{}-gray?style=plastic&logo=lichess&label={}&labelColor=black&color=gray".format(self._format, self._rating, self_.username)
+        return "https://img.shields.io/badge/-{}%3A%20{}-gray?style=plastic&logo=lichess&label={}&labelColor=black&color=gray".format(self._time_format, self._rating, self._username)
     def __repr__(self):
-        return ("Lichess(self._'{self._username}', '{self._time_format')")
+        return ("Lichess(self._'{self._username}', '{self._time_format}')")
     def __str__(self):
         return self._shield
-           
--- update.py
+++ update.py
@@ -1,17 +1,13 @@
 # This script generates new markdown content for my GitHub profile README
 # Eli Bell
 # 2023-10-11
-from lichess import Lichess as lc
-
+from lichess import Lichess as li
 class Badge():
     def __init__(self, name, url):
         self._name = name
         self._url = url
-    def markdown(self):
-        return self._markdown
     def __repr__(self):
         return f"Badge('{self._name}', '{self._url}')"
     def __str__(self):
         return f"![{self._name.upper()}]({self._url})"
-           
-print(Badge("lichess", lc))
+print(Badge("lichess", str(li())))
