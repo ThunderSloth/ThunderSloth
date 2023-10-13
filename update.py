fb42bbf270d8a79e3fa3ad882fe29e5e498b1f03
Eli Bell <elibell@Elis-MacBook-Pro.local>
2023-10-12 23:27:50 -0400

    .

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 # This script generates new markdown content for my GitHub profile README
 # Eli Bell
 # 2023-10-11
-import lichess.Lichess as lc
+from lichess import Lichess as lc
 
 class Badge():
     def __init__(self, name, url):
