af138de81052ed209c06f39af8fbf11c9ef56eed
Eli Bell <elibell@elibell.net>
2023-12-16 00:13:01 -0500

    .

--- update.py
+++ update.py
@@ -3,19 +3,28 @@
 # 2023-10-11
 
 from shields import shields
-from lichess import LC 
-from monkeytype import MT 
+from lichess import LC
+from monkeytype import MT
 from discogs import DC
 
 STYLE = "plastic"
 
-badges = [
+dynamic = [
     LC(),
     MT(),
     DC(),
 ]
 
-for badge in badges:
-    print(shields(*badge.parameters, STYLE))
+static = [
+    "[!python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)",
+    "[!java](https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white)",
+    "[!lua](https://img.shields.io/badge/Lua-2C2D72?style=for-the-badge&logo=lua&logoColor=white)",
+    "[!sqlite](https://img.shields.io/badge/SQLite-07405E?style=for-the-badge&logo=sqlite&logoColor=white)",
+    "[!nvim](https://img.shields.io/badge/NeoVim-%2357A143.svg?&style=for-the-badge&logo=neovim&logoColor=white)",
+    "[!git](https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white)",
+    "[!ai](https://img.shields.io/badge/Adobe%20Illustrator-FF9A00?style=for-the-badge&logo=adobe%20illustrator&logoColor=white)",
+]
 
-print("![Static Badge](https://img.shields.io/badge/nvim-blue?logo=Neovim&labelColor=black&color=%2357A143)")
+[print(badge) for badge in static]
+
+[print(shields(*badge.parameters, STYLE)) for badge in dynamic]
